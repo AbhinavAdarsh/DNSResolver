4b50897696151ae305095cb2e9567512af4616e4
AbhinavAdarsh <31198377+AbhinavAdarsh@users.noreply.github.com>
2018-02-19 18:59:08 -0500

    Made changes and added comments (working dig properly for all queries A, NS, MX)

--- dnsSec.py
+++ dnsSec.py
@@ -1,135 +1,3 @@
-# import dns.name
-# import dns.query
-# import dns.resolver
-# import sys
-# import dnslib
-# import socket
-# import dns.dnssec
-#
-# rootServers = ["198.41.0.4","199.9.14.201","192.33.4.12","199.7.91.13","192.203.230","192.5.5.241","192.112.36.4",
-#                "198.97.190.53","192.36.148.17","192.58.128.30","193.0.14.129","199.7.83.42","202.12.27.33"]
-#
-
-# def getIp(hostname,recordType):
-#
-#     for server in rootServers:
-#         try:
-#             rootQuery = dns.message.make_query(hostname,dns.rdatatype.A,want_dnssec=True)
-#             rootResponse = dns.query.udp(rootQuery,server)
-#             prevDSrecord = rootResponse.authority[1]
-#             print prevDSrecord
-#             dnsKeyQuery = dns.message.make_query(".",dns.rdatatype.DNSKEY,want_dnssec=True)
-#             dnsKeyResponse = dns.query.udp(dnsKeyQuery,server)
-#             #print dnsKeyResponse
-#             # name = dns.name.from_text('.')
-#             # dsRecord = dns.dnssec.make_ds(name,dnsKeyResponse.answer[0])
-#             ans = dnsKeyResponse.answer
-#             rootRes = []
-#
-#             for data in ans[0]:
-#                 if '257' in str(data):
-#                     rootRes.append(str(data))
-#
-#             # Validating root response with DNS Key
-#             if rootKey == rootRes:
-#                 print 'Matched with root'
-#             else:
-#                 print 'DNSSec verification failed'
-#                 exit()
-#
-#             name = dns.name.from_text('.')
-#
-#             # Validating DNSKey with RRSIG
-#             if len(dnsKeyResponse.answer) == 2:
-#                 try:
-#                     dns.dnssec.validate(dnsKeyResponse.answer[0],dnsKeyResponse.answer[1],{name:dnsKeyResponse.answer[0]})
-#                     print "Validated at level 0"
-#                 except dns.dnssec.ValidationFailure:
-#                     print "Validation Failure"
-#             else:
-#                 print "Error in DNS response"
-#             break
-#
-#         except BaseException:
-#             print "No response from server. Querying the next root Server from the list"
-#
-#     servers = rootResponse.additional
-#     validate(hostname,servers,1,prevDSrecord,rootResponse)
-#
-# def validate(hostname,serverlist,length,prevDSrecord,response):
-#
-#     while response.answer != 0 and not response.answer and length < 4:
-#         data = hostname.split('.')
-#         queryName = ''
-#         x = 0
-#         for d in reversed(data):
-#             queryName = str(d) + '.' + queryName
-#             x = x+1
-#             if x == length:
-#                 break
-#
-#         print queryName
-#         #exit()
-#
-#         for server in serverlist:
-#             # try:
-#                 server = str(server)
-#                 data = server.split(' ')
-#
-#                 query = dns.message.make_query(hostname, dns.rdatatype.A, want_dnssec=True)
-#                 response = dns.query.udp(query, data[-1])
-#                 serverlist = response.additional
-#
-#                 print '--------------------------------------------------------'
-#                 # currDSrecord = str(response.authority[1])
-#                 # currDSrecord = currDSrecord.split(' ')
-#                 #print type(currDSrecord[-1])
-#                 keyQuery = dns.message.make_query(queryName, dns.rdatatype.DNSKEY, want_dnssec=True)
-#                 keyResponse = dns.query.udp(keyQuery, data[-1])
-#
-#                 recordName = dns.name.from_text(queryName)
-#                 #algo = dns.dnssec.algorithm_from_text('SHA256')
-#                 print keyResponse.answer[1]
-#                 currDSrecord = dns.dnssec.make_ds(recordName,keyResponse.answer[0],algorithm='SHA256')
-#                 # Validation of DS record, chain of trust between Parent and Child
-#                 print prevDSrecord
-#                 print currDSrecord
-#                 if str(prevDSrecord) == currDSrecord:
-#                     print 'DS record validated between Parent and Child'
-#                 else:
-#                     print 'DNSSec verification failed'
-#
-#                 prevDSrecord = response.authority[1]
-#                 break
-#
-#             #except:
-#                 print "Error"
-#
-#         name = dns.name.from_text(queryName)
-#
-#         if len(keyResponse.answer) == 2:
-#             try:
-#                 dns.dnssec.validate(keyResponse.answer[0], keyResponse.answer[1], {name: keyResponse.answer[0]})
-#                 print "Validated at level" + str(length)
-#             except dns.dnssec.ValidationFailure:
-#                 print "Validation Failure"
-#                 exit()
-#         else:
-#             print "Error in DNS response"
-#             exit()
-#
-#         # validate(hostname,serverlist,length+1,prevDSrecord)
-#         length = length + 1
-#     print 'DONE'
-#
-#
-# def main():
-#     getIp(sys.argv[1],sys.argv[2])
-#
-# if __name__ == '__main__':
-#     main()
-
-
 import dns.name
 import dns.query
 import dns.resolver
@@ -153,7 +21,6 @@ def iterativeResolver(hostname,reqRecord,response):
     x = 0
     length = 0
     while len(serverlist) > 0 and x < 5:
-        ####################
         # Preparing query name for each level starting from root
         period = question.split('.')
         queryName = ''
@@ -166,9 +33,8 @@ def iterativeResolver(hostname,reqRecord,response):
 
         if length == 0:
             queryName = '.'
-        print queryName
+        #print queryName
 
-        ####################
         x = x + 1
         length = length + 1
         for server in serverlist:
@@ -176,20 +42,20 @@ def iterativeResolver(hostname,reqRecord,response):
             data = server.split(' ')
             try:
                 query = dns.message.make_query(hostname,dns.rdatatype.A,want_dnssec=True)
-                print data[-1]
-                print '************'
+                # print data[-1]
+                # print '************'
                 if len(data[-1]) > 16:
                     continue
                 response = dns.query.udp(query,data[-1])
-                print 'length =' + str(len(response.authority))
-                print response.authority[1]
-                # exit()
-                print response.authority[2]
+                # print 'length =' + str(len(response.authority))
+                # print response.authority[1]
+                # # exit()
+                # print response.authority[2]
 
                 ##################
                 keyQuery = dns.message.make_query(queryName,dns.rdatatype.DNSKEY,want_dnssec=True)
                 keyResponse = dns.query.udp(keyQuery,data[-1])
-                print keyResponse
+                # print keyResponse
                 # Storing the KSK keys
                 if queryName == '.':
                     ksk_keys = set()
@@ -211,7 +77,7 @@ def iterativeResolver(hostname,reqRecord,response):
                         print 'KSK Validation done'
                     else:
                         print 'Error in DNS Key response'
-
+                        break
                 if len(prevDSrecord) > 0 and len(keyResponse.answer) != 0:
                     try:
                         for i in range(0,len(keyResponse.answer[0])):
@@ -228,8 +94,8 @@ def iterativeResolver(hostname,reqRecord,response):
                                 # print partprevDSrecord[-1]
                                 # print '##############'
                                 if partcurrentDSRecord[-1] == partprevDSrecord[-1]:
-                                    print 'Validated DS Record'
-                                    break
+                                    #print 'Validated DS Record'
+                                    pass
                     except dns.dnssec.ValidationFailure:
                         print 'DS record not validated'
                 else:
@@ -250,7 +116,6 @@ def iterativeResolver(hostname,reqRecord,response):
                     # print 'Validation done'
                 else:
                     print 'Error in DNS Key validation'
-
                 ##################
                 serverlist = response.additional
                 break
@@ -280,17 +145,17 @@ def main():
     question = sys.argv[1]
     global reqRecord
     reqRecord = sys.argv[2]
+    try:
+        response = iterativeResolver(sys.argv[1], sys.argv[2],'')
+        print 'QUESTION SECTION:'
+        print str(question) + " " + "IN" + " " + str(reqRecord)
 
-    print 'QUESTION SECTION:'
-    print str(question) + " " + "IN" + " " + str(reqRecord)
-
-    print 'ANSWER SECTION:'
-    response = iterativeResolver(sys.argv[1], sys.argv[2],'')
-    # for records in response:
-    #     print type(records)
-    print response[0]
-    print 'Query time: %s ms' % ((time.time() - startTime) * 1000)
-    print 'WHEN: ' + str(time.ctime())
+        print 'ANSWER SECTION:'
+        print response[0]
+        print 'Query time: %s ms' % ((time.time() - startTime) * 1000)
+        print 'WHEN: ' + str(time.ctime())
+    except:
+        print 'Error in Resolution'
 
 if __name__ == '__main__':
     main()
--- graphplot.py
+++ graphplot.py
@@ -2,17 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# data_req = pd.read_table("graphdata.csv", sep=",")
-# #sort values per column
-# sorted_values = data_req.apply(lambda x: x.sort_values())
-
 list1 = ['3.51138114929', '3.47979068756', '3.0944108963', '3.00369262695', '35.6265068054', '3.41827869415', '3.25629711151', '3.30471992493', '3.29790115356', '3.60732078552', '2.9953956604', '3.17871570587', '3.26900482178', '9.00819301605', '3.30619812012', '3.18789482117', '3.84860038757', '16.6954040527', '3.62501144409', '29.0525913239', '4.48920726776', '3.25739383698', '3.06899547577', '3.20329666138', '3.22091579437']
 list2 = ['34.658408165', '23.574590683', '9.14778709412', '150.644493103', '215.436697006', '30.26471138', '22.9000091553', '14.1282081604', '24.5168924332', '8.21309089661', '7.44268894196', '8.09199810028', '24.0535020828', '22.6561069489', '11.0067844391', '8.14161300659', '8.12349319458', '10.1135969162', '7.71009922028', '128.326797485', '9.02740955353', '155.55100441', '21.3997840881', '23.7920999527', '23.943400383']
 list3 = ['158.145999908', '44.3108797073', '68.3996200562', '869.463992119', '409.649515152', '108.475184441', '243.679308891', '303.16259861', '287.589907646', '307.253599167', '34.9226951599', '37.163901329', '195.06611824', '34.4460010529', '106.291007996', '822.36058712', '40.4196023941', '413.941216469', '44.6487903595', '512.017989159', '52.1965026855', '306.631207466', '96.2080001831', '282.847189903', '44.2473888397']
 website = ['wikipedia.org', 'youtube.com', 'live.com', '360.cn', 'jd.com', 'tmall.com', 'google.co.in', 'weibo.com', 'google.com.br', 'baidu.com', 'facebook.com', 'twitter.com', 'google.de', 'reddit.com', 'taobao.com', 'Qq.com', 'amazon.com', 'Vk.com', 'yahoo.com', 'Sina.com.cn', 'instagram.com', 'Sohu.com', 'google.co.uk', 'google.co.jp', 'google.com']
-#plot with matplotlib
-#note that you have to drop the Na's on columns to have appropriate
-#dimensions per variable.
+
 
 list1_float = []
 list2_float = []
@@ -34,7 +28,7 @@ web3 = np.cumsum(list3_float)
 listnum = []
 for i in range(0,25):
     listnum.append(i)
-plt.xticks(listnum,website,rotation=60)
+plt.xticks(listnum,website,rotation=20)
 plt.plot(listnum,web1,c='red',label='Local DNS Resolver')
 plt.plot(listnum,web2,c='green',label='Google DNS Resolver')
 plt.plot(listnum,web3,c='blue',label='My DNS Resolver')
@@ -43,11 +37,6 @@ plt.legend(loc='upper left')
 
 plt.xlabel('Alexa Top 25 Website List')
 plt.ylabel('Resolution Time')
-
 plt.show()
 
-
-# for col in sorted_values.columns:
-#     y = np.linspace(0.,1., len(sorted_values[col].dropna()))
-#     plt.plot(sorted_values[col].dropna(),y)
 
